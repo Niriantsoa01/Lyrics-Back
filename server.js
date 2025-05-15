39705783285a6632fb610a39b9587fd9bed553f6
Niriantsoa01 <niriantsoaherge@gmail.com>
2025-05-15 16:13:48 +0300

    commit de back lyrics app

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(cors());
 
